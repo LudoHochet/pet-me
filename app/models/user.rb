1d83d05bb886b5984c496e8230b919d7c992e962
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-02-17 16:02:37 +0100

    add columns and validates user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :telephone, presence: true
+  validates :description, presence: true
+  validates :email, uniqueness: true
 end

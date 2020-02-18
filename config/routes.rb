c5d7c0281ccdec8e3d9dc063e2341338782c43a8
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-02-18 23:13:58 +0100

    can change status of bookings and add a booking in seeds

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   end
 
   get '/dashboard', to: 'dashboards#show'
+  patch '/bookings/:id/status', to: 'bookings#change_status', as: :switch_bookings_status
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

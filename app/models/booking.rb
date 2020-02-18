41fdae7e7ed210b8833135fce4778983690e8eb3
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-02-18 10:36:33 +0100

    modifie booking model validates

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :pet
   belongs_to :user
   validates :status, default: false
-  validates :start_date, precence: true
+  validates :start_date, presence: true
   validates :end_date, presence: true
 end

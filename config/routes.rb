11932b3ddc92198e665aae190d099207f812e861
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-02-18 12:10:32 +0100

    send .env with Cloudinary key & reduce routes to necessary ones

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,5 @@ Rails.application.routes.draw do
     resources :bookings, only: [ :new, :create, :edit, :update, :show ]
   end
 
-  resources :bookings, only: [ :index, :destroy ]
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

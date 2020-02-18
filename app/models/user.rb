1b89684db850920e3f7de57f3b712a446d309c41
Bertrand Jouin <bertrandjouin@gmail.com>
2020-02-18 12:46:23 +0100

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,5 @@ class User < ApplicationRecord
   has_many :bookings
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :address, presence: true
-  validates :telephone, presence: true
-  validates :description, presence: true
   validates :email, uniqueness: true
 end

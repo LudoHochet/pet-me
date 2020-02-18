c3e1c8f9981fe5f78b627002ce1731f36f3f4ae8
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-02-18 15:30:17 +0100

    add dashboard routes and controller

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,8 @@
+class DashboardsController < ApplicationController
+  def show
+    @user = current_user
+    @pets = current_user.pets
+    @my_bookings = current_user.bookings
+    @my_rented_pets = Booking.where(pet_id: current_user.pet_ids)
+  end
+end

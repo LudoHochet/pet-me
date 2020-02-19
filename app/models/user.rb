a8de5cb636d9ba71190405fd142e74bc291824b7
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-02-19 13:00:03 +0100

    add card and markers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
@@ -11,4 +9,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, uniqueness: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end

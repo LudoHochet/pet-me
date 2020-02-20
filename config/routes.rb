f3201db0d5dfa32058250b2b9c21f0a155f238e0
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-02-20 12:14:19 +0100

    change routes and controller for accept or refuse pending bookings

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   end
 
   get '/dashboard', to: 'dashboards#show'
-  patch '/bookings/:id/status', to: 'bookings#change_status', as: :switch_bookings_status
+
+  patch '/bookings/:id/accept', to: 'bookings#accept', as: :switch_bookings_accept
+  patch '/bookings/:id/deny', to: 'bookings#deny', as: :switch_bookings_deny
+
+
   patch '/pets/:id/status', to: 'pets#change_status', as: :switch_pets_status
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

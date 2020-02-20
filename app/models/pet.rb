d9d17e1ff0e75c3623e6258d003ea58ec2b83082
Bertrand Jouin <bertrandjouin@gmail.com>
2020-02-20 12:23:20 +0100

    petcreationOK

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,5 @@
 class Pet < ApplicationRecord
-  SPECIES = ['cat', 'dog', 'mouse', 'crocodile', 'monkey', 'cow', 'hen', 'goose']
+  SPECIES = ['cat', 'dog', 'mouse', 'crocodile', 'monkey', 'cow', 'hen', 'goose', 'rabbit']
   has_many_attached :photos
   belongs_to :user
   has_many :bookings

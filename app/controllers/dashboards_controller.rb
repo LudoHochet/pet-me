ef40ee6535d0c77053ab83727902595ab8387be5
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-02-21 12:41:48 +0100

    fix bug for switch button

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < ApplicationController
   def show
     @user = current_user
-    @pets = current_user.pets
+    @pets = current_user.pets.order("created_at")
     @my_bookings = current_user.bookings
     @my_rented_pets = Booking.where(pet_id: current_user.pet_ids)
   end
